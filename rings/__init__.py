7676058e3012405c8b7e4d21caaa7150cc2315fa
jeremy-wayland <jeremy.don.wayland@gmail.com>
2025-06-25 14:24:47 +0200

    add perturbations + unit tests

---
+++ rings/__init__.py
@@ -0,0 +1,18 @@
+from rings.perturbations import (
+    Original,
+    EmptyGraph,
+    RandomGraph,
+    RandomFeatures,
+    CompleteFeatures,
+    CompleteGraph,
+)
+
+
+__all__ = [
+    "Original",
+    "EmptyGraph",
+    "RandomGraph",
+    "RandomFeatures",
+    "CompleteFeatures",
+    "CompleteGraph",
+]
